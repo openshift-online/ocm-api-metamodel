9861650c68d5b130ba36d4f1e3cd337ffe19a800
Nimrod Shneor <nimrodshn@gmail.com>
2019-08-11 13:57:46 +0300

    Generate Resource methods for the Resources interfaces.

--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.12
 
 require (
 	github.com/antlr/antlr4 v0.0.0-20181218183524-be58ebffde8e
+	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/onsi/ginkgo v1.8.0
 	github.com/onsi/gomega v1.5.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/pflag v1.0.3
-	golang.org/x/net v0.0.0-20190125091013-d26f9f9a57f3 // indirect
-	golang.org/x/sys v0.0.0-20190201073406-2970a3744de3 // indirect
+	golang.org/x/net v0.0.0-20190322120337-addf6b3196f6 // indirect
+	golang.org/x/sys v0.0.0-20190322080309-f49334f85ddc // indirect
 )
--- pkg/generators/servers.go
+++ pkg/generators/servers.go
@@ -18,6 +18,7 @@ package generators
 
 import (
 	"fmt"
+	"path"
 	"path/filepath"
 
 	"gitlab.cee.redhat.com/service/ocm-api-metamodel/pkg/concepts"
@@ -138,11 +139,11 @@ func (b *ServersGeneratorBuilder) Build() (generator *ServersGenerator, err erro
 func (g *ServersGenerator) Run() error {
 	var err error
 
-	// Generate the Go resource for each model resource:
+	// Generate the Go server for each model resource:
 	for _, service := range g.model.Services() {
 		for _, version := range service.Versions() {
 			for _, resource := range version.Resources() {
-				err = g.generateServerFile(resource)
+				err = g.generateResourceServerFile(resource)
 				if err != nil {
 					return err
 				}
@@ -163,7 +164,7 @@ func (g *ServersGenerator) Run() error {
 	return nil
 }
 
-func (g *ServersGenerator) generateServerFile(resource *concepts.Resource) error {
+func (g *ServersGenerator) generateResourceServerFile(resource *concepts.Resource) error {
 	var err error
 
 	// Calculate the package and file name:
@@ -177,28 +178,174 @@ func (g *ServersGenerator) generateServerFile(resource *concepts.Resource) error
 		Base(g.base).
 		Package(pkgName).
 		File(fileName).
-		Function("resourceName", g.resourceName).
+		Function("methodName", g.methodName).
+		Function("serverName", g.serverName).
+		Function("fieldName", g.fieldName).
+		Function("fieldType", g.fieldType).
+		Function("getterName", g.getterName).
+		Function("getterType", g.getterType).
+		Function("requestName", g.requestName).
+		Function("responseName", g.responseName).
+		Function("methodName", g.methodName).
+		Function("requestParameters", g.requestParameters).
+		Function("responseParameters", g.responseParameters).
+		Function("setterName", g.setterName).
+		Function("setterType", g.setterType).
+		Function("zeroValue", g.types.ZeroValue).
 		Build()
 	if err != nil {
 		return err
 	}
 
 	// Generate the source:
-	g.generateServerSource(resource)
+	g.generateResourceServerSource(resource)
 
 	// Write the generated code:
 	return g.buffer.Write()
 }
 
-func (g *ServersGenerator) generateServerSource(resource *concepts.Resource) {
+func (g *ServersGenerator) generateResourceServerSource(resource *concepts.Resource) {
 	g.buffer.Emit(`
-		{{ $resourceName := resourceName .Resource }}
+		{{ $serverName := serverName .Resource }}
 
-		// {{ $resourceName }}Server represents the interface the manages the '{{ .Resource.Name }}' resource.
-		type {{ $resourceName }}Server interface {}
+		// {{ $serverName }}Server represents the interface the manages the '{{ .Resource.Name }}' resource.
+		type {{ $serverName }}Server interface {
+			{{ range .Resource.Methods }}
+				{{ $methodName := methodName . }}
+				{{ $responseName := responseName . }}
+				{{ $requestName := requestName . }}
+
+				{{ $methodName }}(request *{{$requestName}}, response *{{$responseName}}) error
+			{{ end }}
+		}
 		`,
 		"Resource", resource,
 	)
+
+	// Generate the request and response types:
+	for _, method := range resource.Methods() {
+		g.generateRequestSource(method)
+		g.generateResponseSource(method)
+	}
+}
+
+func (g *ServersGenerator) generateRequestSource(method *concepts.Method) {
+	g.buffer.Import("bytes", "")
+	g.buffer.Import("context", "")
+	g.buffer.Import("encoding/json", "")
+	g.buffer.Import("fmt", "")
+	g.buffer.Import("io/ioutil", "")
+	g.buffer.Import("net/http", "")
+	g.buffer.Import("net/url", "")
+	g.buffer.Import(path.Join(g.base, g.errorsPkg()), "")
+	g.buffer.Import(path.Join(g.base, g.helpersPkg()), "")
+	g.buffer.Emit(`
+		{{ $requestName := requestName .Method }}
+		{{ $requestParameters := requestParameters .Method }}
+
+		// {{ $requestName }} is the request for the '{{ .Method.Name }}' method.
+		type {{ $requestName }} struct {
+			path      string
+			query     url.Values
+			{{ range $requestParameters }}
+				{{ fieldName . }} {{ fieldType . }}
+			{{ end }}
+		}
+
+		{{ range $requestParameters }}
+			{{ $parameterType := .Type.Name.String }}
+			{{ $fieldName := fieldName . }}
+			{{ $getterName := getterName . }}
+			{{ $getterType := getterType . }}
+
+			// {{ $getterName }} returns the value of the '{{ .Name }}' parameter.
+			//
+			{{ lineComment .Doc }}
+			func (r *{{ $requestName }}) {{ $getterName }}() {{ $getterType }} {
+				{{ if or .Type.IsStruct .Type.IsList .Type.IsMap }}
+					if r == nil {
+						return nil
+					}
+					return r.{{ $fieldName }}
+				{{ else }}
+					if r != nil && r.{{ $fieldName }} != nil {
+						return *r.{{ $fieldName }}
+					}
+					return {{ zeroValue .Type }}
+				{{ end }}
+			}
+
+			// Get{{ $getterName }} returns the value of the '{{ .Name }}' parameter and
+			// a flag indicating if the parameter has a value.
+			//
+			{{ lineComment .Doc }}
+			func (r *{{ $requestName }}) Get{{ $getterName }}() (value {{ $getterType }}, ok bool) {
+				ok = r != nil && r.{{ $fieldName }} != nil
+				if ok {
+					{{ if or .Type.IsStruct .Type.IsList .Type.IsMap }}
+						value = r.{{ $fieldName }}
+					{{ else }}
+						value = *r.{{ $fieldName }}
+					{{ end }}
+				}
+				return
+			}
+		{{ end }}
+		`,
+		"Method", method,
+	)
+}
+
+func (g *ServersGenerator) generateResponseSource(method *concepts.Method) {
+	g.buffer.Import("io", "")
+	g.buffer.Import("net/http", "")
+	g.buffer.Import(path.Join(g.base, g.errorsPkg()), "")
+	g.buffer.Emit(`
+		{{ $responseName := responseName .Method }}
+		{{ $responseParameters := responseParameters .Method }}
+
+		// {{ $responseName }} is the response for the '{{ .Method.Name }}' method.
+		type  {{ $responseName }} struct {
+			status int
+			err    *errors.Error
+			{{ range $responseParameters }}
+				{{ fieldName . }} {{ fieldType . }}
+			{{ end }}
+		}
+		
+		
+		{{ range $responseParameters }}
+			{{ $fieldName := fieldName . }}
+			{{ $setterName := setterName . }}
+			{{ $setterType := setterType . }}
+
+			// {{ $setterName }} sets the value of the '{{ .Name }}' parameter.
+			//
+			{{ lineComment .Doc }}
+			func (r *{{ $responseName }}) {{ $setterName }}(value {{ $setterType }}) *{{ $responseName }} {
+				{{ if or .Type.IsStruct .Type.IsList }}
+					r.{{ $fieldName }} = value
+				{{ else }}
+					r.{{ $fieldName }} = &value
+				{{ end }}
+				return r
+			}
+		{{ end }}
+		`,
+		"Method", method,
+	)
+}
+
+func (g *ServersGenerator) errorsPkg() string {
+	return g.names.Package(nomenclator.Errors)
+}
+
+func (g *ServersGenerator) helpersPkg() string {
+	return g.names.Package(nomenclator.Helpers)
+}
+
+func (g *ServersGenerator) serversFile() string {
+	return g.names.File(nomenclator.Servers)
 }
 
 func (g *ServersGenerator) pkgName(version *concepts.Version) string {
@@ -211,6 +358,96 @@ func (g *ServersGenerator) fileName(resource *concepts.Resource) string {
 	return g.names.File(names.Cat(resource.Name(), nomenclator.Server))
 }
 
-func (g *ServersGenerator) resourceName(resource *concepts.Resource) string {
+func (g *ServersGenerator) serverName(resource *concepts.Resource) string {
 	return g.names.Public(resource.Name())
+}
+
+func (g *ServersGenerator) methodName(method *concepts.Method) string {
+	return g.names.Public(method.Name())
+}
+
+func (g *ServersGenerator) requestName(method *concepts.Method) string {
+	name := names.Cat(method.Owner().Name(), method.Name(), nomenclator.Server, nomenclator.Request)
+	return g.names.Public(name)
+}
+
+func (g *ServersGenerator) requestParameters(method *concepts.Method) []*concepts.Parameter {
+	result := make([]*concepts.Parameter, 0)
+	for _, parameter := range method.Parameters() {
+		if parameter.In() {
+			result = append(result, parameter)
+		}
+	}
+	return result
+}
+
+func (g *ServersGenerator) responseName(method *concepts.Method) string {
+	name := names.Cat(method.Owner().Name(), method.Name(), nomenclator.Server, nomenclator.Response)
+	return g.names.Public(name)
+}
+
+func (g *ServersGenerator) responseParameters(method *concepts.Method) []*concepts.Parameter {
+	result := make([]*concepts.Parameter, 0)
+	for _, parameter := range method.Parameters() {
+		if parameter.Out() {
+			result = append(result, parameter)
+		}
+	}
+	return result
+}
+
+func (g *ServersGenerator) fieldName(parameter *concepts.Parameter) string {
+	name := g.names.Private(parameter.Name())
+	name = g.avoidBuiltin(name, builtinFields)
+	return name
+}
+
+func (g *ServersGenerator) fieldType(parameter *concepts.Parameter) *golang.TypeReference {
+	return g.types.NullableReference(parameter.Type())
+}
+
+func (g *ServersGenerator) getterName(parameter *concepts.Parameter) string {
+	name := g.names.Public(parameter.Name())
+	name = g.avoidBuiltin(name, builtinGetters)
+	return name
+}
+
+func (g *ServersGenerator) getterType(parameter *concepts.Parameter) *golang.TypeReference {
+	return g.accessorType(parameter.Type())
+}
+
+func (g *ServersGenerator) setterName(parameter *concepts.Parameter) string {
+	name := g.names.Public(parameter.Name())
+	name = g.avoidBuiltin(name, builtinSetters)
+	return name
+}
+
+func (g *ServersGenerator) setterType(parameter *concepts.Parameter) *golang.TypeReference {
+	return g.accessorType(parameter.Type())
+}
+
+func (g *ServersGenerator) accessorType(typ *concepts.Type) *golang.TypeReference {
+	switch {
+	case typ.IsList():
+		element := typ.Element()
+		switch {
+		case element.IsStruct():
+			name := g.names.Public(names.Cat(element.Name(), nomenclator.List))
+			return g.types.Reference("", "", "", "*"+name)
+		default:
+			return g.types.NullableReference(typ)
+		}
+	case typ.IsStruct():
+		return g.types.NullableReference(typ)
+	default:
+		return g.types.ValueReference(typ)
+	}
+}
+
+func (g *ServersGenerator) avoidBuiltin(name string, builtins map[string]interface{}) string {
+	_, ok := builtins[name]
+	if ok {
+		name = name + "_"
+	}
+	return name
 }
