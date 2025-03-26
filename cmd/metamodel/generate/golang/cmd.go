bfea60cecd7228a63c23b59f35bc3e16b58d5cfd
David Eads <deads@redhat.com>
2025-03-26 11:24:59 -0400

    make it possible to run specific generators

--- cmd/metamodel/generate/golang/cmd.go
+++ cmd/metamodel/generate/golang/cmd.go
@@ -43,9 +43,27 @@ var Cmd = &cobra.Command{
 
 // Values of the command line arguments:
 var args struct {
-	paths  []string
-	base   string
-	output string
+	paths      []string
+	base       string
+	apiBase    string
+	output     string
+	generators []string
+}
+
+var knownGenerators = map[string]bool{
+	"*":              true,
+	"builders":       true,
+	"builders-alias": true,
+	"clients":        true,
+	"errors":         true,
+	"helpers":        true,
+	"json":           true,
+	"json-alias":     true,
+	"request-json":   true,
+	"metrics":        true,
+	"openapi":        true,
+	"types":          true,
+	"types-alias":    true,
 }
 
 func init() {
@@ -66,11 +84,38 @@ func init() {
 		"Import path of the base package for the generated code.",
 	)
 	flags.StringVar(
+		&args.apiBase,
+		"apiBase",
+		"",
+		"Import path of the base package for the generated API code.",
+	)
+	flags.StringVar(
 		&args.output,
 		"output",
 		"",
 		"Directory where the source code will be generated.",
 	)
+	flags.StringSliceVar(
+		&args.generators,
+		"generators",
+		[]string{"*"},
+		"List of generators to run.  Default is * which is all of them.",
+	)
+}
+
+func shouldRunGenerator(generator string) bool {
+	if _, known := knownGenerators[generator]; !known {
+		panic(fmt.Sprintf("coding error: generator '%s' not known", generator))
+	}
+	for _, currGenerator := range args.generators {
+		if currGenerator == "*" {
+			return true
+		}
+		if currGenerator == generator {
+			return true
+		}
+	}
+	return false
 }
 
 func run(cmd *cobra.Command, argv []string) {
@@ -95,6 +140,16 @@ func run(cmd *cobra.Command, argv []string) {
 		reporter.Errorf("Option '--output' is mandatory")
 		ok = false
 	}
+	if len(args.generators) == 0 {
+		reporter.Errorf("Option '--generators' is mandatory")
+		ok = false
+	}
+	for _, generator := range args.generators {
+		if !knownGenerators[generator] {
+			reporter.Errorf("value %q for '--generators' is unknown", generator)
+			ok = false
+		}
+	}
 	if !ok {
 		os.Exit(1)
 	}
@@ -113,6 +168,7 @@ func run(cmd *cobra.Command, argv []string) {
 	goPackagesCalculator, err := golang.NewPackagesCalculator().
 		Reporter(reporter).
 		Base(args.base).
+		APIBase(args.apiBase).
 		Build()
 	if err != nil {
 		reporter.Errorf("Can't create Go packages calculator: %v", err)
@@ -153,126 +209,213 @@ func run(cmd *cobra.Command, argv []string) {
 	var gens []generators.Generator
 	var gen generators.Generator
 
-	// Create the errors generator:
-	gen, err = golang.NewErrorsGenerator().
-		Reporter(reporter).
-		Model(model).
-		Output(args.output).
-		Packages(goPackagesCalculator).
-		Names(goNamesCalculator).
-		Build()
-	if err != nil {
-		reporter.Errorf("Can't create errors generator: %v", err)
-		os.Exit(1)
+	if shouldRunGenerator("errors") {
+		// Create the errors generator:
+		gen, err = golang.NewErrorsGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create errors generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
 	}
-	gens = append(gens, gen)
 
-	// Create the helpers generator:
-	gen, err = golang.NewHelpersGenerator().
-		Reporter(reporter).
-		Model(model).
-		Output(args.output).
-		Packages(goPackagesCalculator).
-		Names(goNamesCalculator).
-		Build()
-	if err != nil {
-		reporter.Errorf("Can't create helpers generator: %v", err)
-		os.Exit(1)
+	if shouldRunGenerator("helpers") {
+		// Create the helpers generator:
+		gen, err = golang.NewHelpersGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create helpers generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
 	}
-	gens = append(gens, gen)
 
-	// Create the types generator:
-	gen, err = golang.NewTypesGenerator().
-		Reporter(reporter).
-		Model(model).
-		Output(args.output).
-		Packages(goPackagesCalculator).
-		Names(goNamesCalculator).
-		Types(goTypesCalculator).
-		Binding(bindingCalculator).
-		Build()
-	if err != nil {
-		reporter.Errorf("Can't create types generator: %v", err)
-		os.Exit(1)
+	if shouldRunGenerator("types") {
+		// Create the types generator:
+		gen, err = golang.NewTypesGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Types(goTypesCalculator).
+			Binding(bindingCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create types generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
 	}
-	gens = append(gens, gen)
 
-	// Create the builders generator:
-	gen, err = golang.NewBuildersGenerator().
-		Reporter(reporter).
-		Model(model).
-		Output(args.output).
-		Packages(goPackagesCalculator).
-		Names(goNamesCalculator).
-		Types(goTypesCalculator).
-		Build()
-	if err != nil {
-		reporter.Errorf("Can't create builders generator: %v", err)
-		os.Exit(1)
+	if len(args.apiBase) > 0 && shouldRunGenerator("types-alias") {
+		// Create the types generator:
+		gen, err = golang.NewTypesAliasGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Types(goTypesCalculator).
+			Binding(bindingCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create types generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
 	}
-	gens = append(gens, gen)
 
-	// Create the clients generator:
-	gen, err = golang.NewClientsGenerator().
-		Reporter(reporter).
-		Model(model).
-		Output(args.output).
-		Packages(goPackagesCalculator).
-		Names(goNamesCalculator).
-		Types(goTypesCalculator).
-		Binding(bindingCalculator).
-		Build()
-	if err != nil {
-		reporter.Errorf("Can't create clients generator: %v", err)
-		os.Exit(1)
+	if shouldRunGenerator("builders") {
+		// Create the builders generator:
+		gen, err = golang.NewBuildersGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Types(goTypesCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create builders generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
 	}
-	gens = append(gens, gen)
 
-	// Create the JSON support generator:
-	gen, err = golang.NewJSONSupportGenerator().
-		Reporter(reporter).
-		Model(model).
-		Output(args.output).
-		Packages(goPackagesCalculator).
-		Names(goNamesCalculator).
-		Types(goTypesCalculator).
-		Binding(bindingCalculator).
-		Build()
-	if err != nil {
-		reporter.Errorf("Can't create JSON support generator: %v", err)
-		os.Exit(1)
+	if shouldRunGenerator("builders-alias") {
+		// Create the builders generator:
+		gen, err = golang.NewBuildersAliasGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Types(goTypesCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create builders alias generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
 	}
-	gens = append(gens, gen)
 
-	// Create the metrics support generator:
-	gen, err = golang.NewMetricsSupportGenerator().
-		Reporter(reporter).
-		Model(model).
-		Output(args.output).
-		Packages(goPackagesCalculator).
-		Names(goNamesCalculator).
-		Binding(bindingCalculator).
-		Build()
-	if err != nil {
-		reporter.Errorf("Can't create metrics support generator: %v", err)
-		os.Exit(1)
+	if shouldRunGenerator("clients") {
+		// Create the clients generator:
+		gen, err = golang.NewClientsGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Types(goTypesCalculator).
+			Binding(bindingCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create clients generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
 	}
-	gens = append(gens, gen)
 
-	// Create the OpenAPI specifications generator:
-	gen, err = golang.NewOpenAPIGenerator().
-		Reporter(reporter).
-		Model(model).
-		Output(args.output).
-		Packages(goPackagesCalculator).
-		Names(openapiNamesCalculator).
-		Binding(bindingCalculator).
-		Build()
-	if err != nil {
-		reporter.Errorf("Can't create OpenAPI specifications generator: %v", err)
-		os.Exit(1)
+	if shouldRunGenerator("json") {
+		// Create the JSON support generator:
+		gen, err = golang.NewJSONSupportGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Types(goTypesCalculator).
+			Binding(bindingCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create JSON support generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
 	}
-	gens = append(gens, gen)
+
+	if shouldRunGenerator("json-alias") {
+		// Create the JSON support generator:
+		gen, err = golang.NewJSONSupportAliasGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Types(goTypesCalculator).
+			Binding(bindingCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create JSON alias support generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
+	}
+
+	if shouldRunGenerator("request-json") {
+		// Create the JSON support generator:
+		gen, err = golang.NewRequestJSONSupportGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Types(goTypesCalculator).
+			Binding(bindingCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create JSON support generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
+	}
+
+	if shouldRunGenerator("metrics") {
+		// Create the metrics support generator:
+		gen, err = golang.NewMetricsSupportGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(goNamesCalculator).
+			Binding(bindingCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create metrics support generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
+	}
+
+	if shouldRunGenerator("openapi") {
+		// Create the OpenAPI specifications generator:
+		gen, err = golang.NewOpenAPIGenerator().
+			Reporter(reporter).
+			Model(model).
+			Output(args.output).
+			Packages(goPackagesCalculator).
+			Names(openapiNamesCalculator).
+			Binding(bindingCalculator).
+			Build()
+		if err != nil {
+			reporter.Errorf("Can't create OpenAPI specifications generator: %v", err)
+			os.Exit(1)
+		}
+		gens = append(gens, gen)
+	}
 
 	// Run the generators:
 	for _, gen := range gens {
